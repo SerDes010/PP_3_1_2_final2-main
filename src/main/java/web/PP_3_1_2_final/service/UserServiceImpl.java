5de50f9c62355f5f127f8183c8eeefceae662b57
SerDes010 <sdesyatoff@gmail.com>
2022-11-25 18:26:07 +0300

    очередное улучшение:)

--- src/main/java/web/PP_3_1_2_final/service/UserServiceImpl.java
+++ src/main/java/web/PP_3_1_2_final/service/UserServiceImpl.java
@@ -11,12 +11,12 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Service
-@Transactional
+
 public class UserServiceImpl implements UserService {
 
     private final UserDAO userDAO;
 
-    @Autowired
+
     public UserServiceImpl(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
@@ -26,21 +26,22 @@ public class UserServiceImpl implements UserService {
         return userDAO.getAllUsers();
     }
 
+
     @Override
     public User getUserById(long id) {
         return userDAO.getUserById(id);
     }
-
+    @Transactional
     @Override
     public void addUser(User user) {
         userDAO.addUser(user);
     }
-
+    @Transactional
     @Override
     public void removeUser(long id) {
         userDAO.removeUser(id);
     }
-
+    @Transactional
     @Override
     public void updateUser(@Valid User user) {
         userDAO.updateUser(user);
